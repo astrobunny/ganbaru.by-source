700cc27347cc51584df30957d158522544efa522
David Siaw <davidsiaw@gmail.com>
2016-12-15 15:33:38 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+var path = require('path');
+
+module.exports = {
+    entry: __dirname + "/js/entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['latest','stage-2']
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['latest','react','stage-2']
+                }
+            }
+        ]
+    },
+    resolve: {
+        modulesDirectories: [
+            __dirname + "/node_modules/",
+            __dirname + "/js/",
+            __dirname + "/css/"
+        ]
+    }
+};
